c674b97a898c512b90b6e99e040280c5cca4c082
Miklyxamaklaiy <105585819+Miklyxamaklaiy@users.noreply.github.com>
2022-08-19 12:57:55 +0300

    Initial commit

---
+++ src/main/java/com/bileter/bilterapi/BilterapiApplication.java
@@ -0,0 +1,15 @@
+package com.bileter.bilterapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilterapiApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilterapiApplication.class, args);
+	}
+
+}
