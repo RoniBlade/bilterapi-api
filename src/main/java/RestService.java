ec144049e63b2307ad0f1add06ea711aeb8388dd
Miklyxamaklaiy <105585819+Miklyxamaklaiy@users.noreply.github.com>
2022-08-20 17:29:47 +0300

    Commit

---
+++ src/main/java/RestService.java
@@ -0,0 +1,42 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class RestService {
+
+    @GetMapping()
+    public String Main()
+    {
+
+
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        String resourceUrl
+//                = "https://api.bileter.ru/ekp";
+//        String requestJson = "{\n" +
+//                "    \"auth\": \"uuSbAIbTdfe3RtAgwbgUliBlChZNhr5eblaFdv9G\",\n" +
+//                "    \"id\": \"123456\",\n" +
+//                "    \"jsonrpc\": \"2.0\",\n" +
+//                "    \"method\": \"changes-show\",\n" +
+//                "    \"params\": {\n" +
+//                "        \"BeginTime\":\"2020-01-24 00:00:00\",\n" +
+//                "        \"EndTime\":\"2020-01-24 23:59:59\"\n" +
+//                "    }\n" +
+//                "}";
+//        // Fetch JSON response as String wrapped in ResponseEntity
+//        ResponseEntity<String> response
+//                = restTemplate.postForEntity(resourceUrl, requestJson, String.class);
+//
+//        String productsJson = response.getBody();
+//
+//        System.out.println(productsJson);
+        return "index";
+
+    }
+
+}
