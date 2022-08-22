82b080b9aa50d5d36d75eef1d77e9967b74323e5
Miklyxamaklaiy <105585819+Miklyxamaklaiy@users.noreply.github.com>
2022-08-22 22:12:34 +0300

    Update version 3.0

---
+++ src/main/java/com/bileter/bilterapi/bd/Storage.java
@@ -0,0 +1,16 @@
+package com.bileter.bilterapi.bd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+
+    List<String> list = new ArrayList<>();
+
+    public void add(String str){
+
+        list.add(str);
+
+    }
+
+}
---
+++ src/main/java/com/bileter/bilterapi/service/SchangesShowController.java
@@ -0,0 +1,36 @@
+package com.bileter.bilterapi.service;
+
+import io.codejournal.maven.swagger2java.model.DataOfBuildings;
+import io.codejournal.maven.swagger2java.model.DataOfBuildingsAnsw;
+import io.codejournal.maven.swagger2java.model.Params;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SchangesShowController {
+
+
+    public String ChangesShow()
+    {
+
+        String requestJson = "{\n" +
+                "    \"auth\": \"uuSbAIbTdfe3RtAgwbgUliBlChZNhr5eblaFdv9G\",\n" +
+                "    \"id\": \"123456\",\n" +
+                "    \"jsonrpc\": \"2.0\",\n" +
+                "    \"method\": \"changes-show\",\n" +
+                "    \"params\": {\n" +
+                "        \"BeginTime\":\"2020-01-24 00:00:00\",\n" +
+                "        \"EndTime\":\"2020-01-24 23:59:59\"\n" +
+                "    }\n" +
+                "}";
+
+
+        return requestJson;
+
+
+    }
+
+}
+
